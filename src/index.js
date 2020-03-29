793135de2ad1e76ea7eef77216075a8dd05c4420
Adelys Alberto <adalbeca@gmail.com>
2020-03-28 23:14:09 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
 import morgan from 'morgan';
 
 dotenv.config();
@@ -14,6 +14,18 @@ app.use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: true }))
     .use(morgan('tiny'));
 
+const whitelist = [ 'http://localhost:4200', 'http://example2.com' ];
+app.use(cors({
+    origin(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (whitelist.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not '
+                  + 'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+}));
 
 app.get('/', (req, res) => {
     res.send('Hi People!');
